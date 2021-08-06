0ef17e6c14ba0b068b215ba9fee30ffd8c1fb8ed
finercapt <rahulkottikasala@gmail.com>
2021-08-06 20:55:30 +0400

    Data added to Admin page from Database

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,13 +1,20 @@
 var db = require('../config/conection')
+var collections = require('../config/collections')
 
 module.exports = {
     addProduct: (product,callback) => {
-        // console.log(product);
+      
 
         db.get().collection('product').insertOne(product).then((data) => {
-            // console.log(data.insertedId);
             callback(data.insertedId)
         })
         
+    },
+    getAllProducts :  () => {
+        return new Promise(async(resolve,reject) => {
+            let products = await db.get().collection(collections.PRODUCT_COLLECTION).find().toArray()
+            console.log(products);
+            resolve(products)
+        })
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -4,60 +4,32 @@ var router = express.Router();
 var productHelpers = require('../helpers/product-helpers')
 
 /* GET users listing. */
-router.get('/',function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  let Product = [
-    {
-      name : 'Iphone 11 ',
-      category : 'Mobile',
-      description : 'This is good phone',
-      image : 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone11-green-select-2019_GEO_EMEA?wid=834&hei=1000&fmt=jpeg&qlt=95&.v=1567021766404'
-    
-    },
-    {
-      name : 'Samsung',
-      category : 'Mobile',
-      description : 'This is good phone',
-      image : 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone11-green-select-2019_GEO_EMEA?wid=834&hei=1000&fmt=jpeg&qlt=95&.v=1567021766404'
-    
-    },
-    {
-      name : 'Honor',
-      category : 'Mobile',
-      description : 'This is good phone',
-      image : 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone11-green-select-2019_GEO_EMEA?wid=834&hei=1000&fmt=jpeg&qlt=95&.v=1567021766404'
-    
-    },
-    {
-      name : 'MI Redmi',
-      category : 'Mobile',
-      description : 'This is good phone',
-      image : 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone11-green-select-2019_GEO_EMEA?wid=834&hei=1000&fmt=jpeg&qlt=95&.v=1567021766404'
-    
-    }
-    
-    ]
+  productHelpers.getAllProducts().then((products) => {
+    res.render('admin/view-product', { admin: true,products})
+  })
 
-  res.render('admin/view-product',{admin:true ,Product})
+
 });
-router.get('/add-product',(req,res) => {
-  res.render('admin/add-product',{admin:true})
+router.get('/add-product', (req, res) => {
+  res.render('admin/add-product', { admin: true })
 });
-router.post('/add-product',(req,res) => {
-  
-  // console.log(req.body);
-  // console.log(req.files.Image);
-  productHelpers.addProduct(req.body,(id) => {
+router.post('/add-product', (req, res) => {
+
+  productHelpers.addProduct(req.body, (id) => {
+
+    // The name of the image has been changed to mongodb objectId
     var image = req.files.Image
     console.log(id);
-    image.mv('./public/images/product-images/'+id+'.jpg',(err,done) => {
-      if(!err){
-        res.render('admin/add-product')
-      }else{
+    image.mv('./public/images/product-images/' + id + '.jpg', (err) => {
+      if (!err) {
+        res.render('admin/add-product',{ admin: true})
+      } else {
         console.log(err);
       }
     })
-    
+
   })
 })
 
